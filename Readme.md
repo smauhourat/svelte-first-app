89f432ebe7299cec8bbbb5e37af01d3e71f9e997
Santiago Mauhourat <smauhourat@aadim.org.ar>
2022-08-04 18:38:46 -0300

    initial

---
+++ Readme.md
@@ -0,0 +1,2 @@
+npm WARN deprecated rollup-plugin-node-resolve@4.2.4: This package has been deprecated and is no longer maintained. Please use @rollup/plugin-node-resolve.
+npm WARN deprecated rollup-plugin-commonjs@9.3.4: This package has been deprecated and is no longer maintained. Please use @rollup/plugin-commonjs.
---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+  let name = "San";
+  let age = 30;
+
+  $: uppercaseName = name.toUpperCase();
+
+  $: console.log(name);
+
+  $: if (name === "Santiago") {
+    console.log("Its run!");
+    age = 20;
+  }
+
+  function changeName() {
+    name = "Santiago";
+  }
+
+  function incrementAge() {
+    age += 1;
+  }
+
+  function handleInput(event) {
+    name = event.target.value;
+  }
+</script>
+
+<h1>Hello {uppercaseName}, my age is {age}!</h1>
+<input type="text" value={name} on:input={handleInput} />
+<input type="text" bind:value={name} />
+<button on:click={incrementAge}>Change Age</button>
+<button on:click={changeName}>Change Name</button>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
