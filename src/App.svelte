59c7e004ae427241b4d175bd52294ece1fe47851
SMauhourat <santiagomauhourat@hotmail.com>
2022-08-04 21:10:48 -0300

    card

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,20 @@
 <script>
-  let name = "San";
+  import ContacCard from './ContacCard.svelte';
+
+  let name = 'San';
   let age = 30;
 
   $: uppercaseName = name.toUpperCase();
 
   $: console.log(name);
 
-  $: if (name === "Santiago") {
-    console.log("Its run!");
+  $: if (name === 'Santiago') {
+    console.log('Its run!');
     age = 20;
   }
 
   function changeName() {
-    name = "Santiago";
+    name = 'Santiago';
   }
 
   function incrementAge() {
@@ -29,6 +31,8 @@
 <input type="text" bind:value={name} />
 <button on:click={incrementAge}>Change Age</button>
 <button on:click={changeName}>Change Name</button>
+
+<ContacCard />
 
 <style>
   h1 {
